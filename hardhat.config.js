ce89c520fe2b972889094d91271d1dc0a44a1fd4
karthik340 <nallaboluk@gmail.com>
2022-07-08 09:34:17 +0530

    1.Added images metadata. 2.Added setting baseURI as metadata ipfs url 3.Added minting function , mintPrice , Total supply 4.Added verification of code on ether scan

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,8 @@
  require("@nomiclabs/hardhat-ethers");
  require("./scripts/deploy.js")
  require("./scripts/mint.js")
-const {ALCHEMY_KEY,ACCOUNT_PRIVATE_KEY} = process.env
+ require("@nomiclabs/hardhat-etherscan");
+const {ALCHEMY_KEY,ACCOUNT_PRIVATE_KEY,ETHERSCAN_API_KEY} = process.env;
 module.exports = {
   solidity: "0.8.1",
   defaultNetwork: "rinkeby",
@@ -20,5 +21,8 @@ module.exports = {
       url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
       accounts: [`0x${ACCOUNT_PRIVATE_KEY}`]
     },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   },
 };
